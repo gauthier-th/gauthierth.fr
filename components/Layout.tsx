552b4c1367088537fecf9ad0bc7104c45fae4505
gauthier-th <mail@gauthier-thomas.dev>
2021-04-25 15:49:26 +0200

    Create homepage tiles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import { motion, Variants, Transition } from 'framer-motion'
@@ -14,46 +14,55 @@ type Props = {
 
 const sealTransition: Transition = {
   type: "tween",
-  ease: "easeInOut",
-  duration: 0.3
+  ease: "anticipate",
+  duration: 0.4
 }
 
 const Layout = ({ children, pageTitle, title, isIndex }: Props) => {
-  return <div>
+  return <div className={styles.container}>
     <Head>
       <title>{pageTitle ? pageTitle.trim() + " | Gauthier's website" : "Gauthier's website"}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header className={styles.header}>
-      <div className={styles.gradient} />
-    </header>
-    <div className='container'>
-      <div className={styles.topbar}>
-        <div>
-          <Link href="/">
-            <a className={styles.gauthierth}>
-              {!isIndex && <motion.img src="/seal.jpg" layoutId="seal-img" transition={sealTransition} />}
-              <motion.h2 layoutId="topbar-title">gauthier-th</motion.h2>
-            </a>
-          </Link>
-        </div>
-        <a className={styles.githubButton} href="https://github.com/gauthier-th" target="_blank">
+    <div className={styles.content}>
+      <header className={styles.header}>
+        <div className={styles.gradient} />
+      </header>
+      <div className='container'>
+        <div className={styles.topbar}>
           <div>
-            <GitHub color="white" />
-            <span>GitHub</span>
+            <Link href="/">
+              <a className={styles.gauthierth}>
+                {!isIndex && <motion.img src="/seal.jpg" layoutId="seal-img" transition={sealTransition} />}
+                <motion.h2 layoutId="topbar-title">gauthier-th</motion.h2>
+              </a>
+            </Link>
           </div>
-          <ChevronRight color="white" />
-        </a>
+          <a className={styles.githubButton} href="https://github.com/gauthier-th" target="_blank">
+            <div>
+              <GitHub color="white" />
+              <span>GitHub</span>
+            </div>
+            <ChevronRight color="white" className={styles.chevronRight} />
+          </a>
+        </div>
+        <div className={styles.title}>
+          <TitleTransition key={title}>
+            <h1>{title}</h1>
+          </TitleTransition>
+          {isIndex && <motion.img src="/seal.jpg" layoutId="seal-img" transition={sealTransition} />}
+        </div>
+        {children}
       </div>
-      <div className={styles.title}>
-        <TitleTransition key={title}>
-          <h1>{title}</h1>
-        </TitleTransition>
-        {isIndex && <motion.img src="/seal.jpg" layoutId="seal-img" transition={sealTransition} />}
-      </div>
-      {children}
     </div>
+    <footer className={styles.footer}>
+      <a href="https://github.com/gauthier-th" target="_blank">
+        Made with{' '}
+        <img src="https://twemoji.maxcdn.com/svg/2764.svg" alt="Seal Image" className={styles.heart} />
+        {' '}by me.
+      </a>
+    </footer>
   </div>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,91 @@
+import { useState, ReactNode } from 'react'
 import Link from 'next/link'
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion"
+import { GitHub, Archive, Mail } from 'react-feather'
 import styles from '../styles/index.module.css'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Welcome on my website!" isIndex>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+type Tile = {
+  icon: ReactNode
+  desc: string,
+  href: string,
+  blank?: boolean
+}
+
+const tiles: Tile[] = [
+  {
+    icon: <GitHub color="black" />,
+    desc: "Follow me on GitHub",
+    href: "https://github.com/gauthier-th",
+    blank: true
+  },
+  {
+    icon: <Archive color="black" />,
+    desc: "Check my projects",
+    href: "/projects"
+  },
+  {
+    icon: <Mail color="black" />,
+    desc: "How to contact me",
+    href: "/contact"
+  }
+]
+
+const TileContent = (props: { icon: ReactNode, desc: string, hoveredTile: boolean }) => {
+  const { icon, desc, hoveredTile } = props
+  return <div>
+    <div className={styles.tile}>
+      {icon}
+      <span>
+        {desc}
+      </span>
+    </div>
+    <AnimatePresence>
+      {hoveredTile && (
+        <motion.div
+          className={styles.tileHovered}
+          layoutId="tile-hovered"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      )}
+    </AnimatePresence>
+  </div>
+}
+
+const IndexPage = () => {
+  const [hoveredTile, setOveredTile] = useState<number|null>(null)
+  return <Layout title="Welcome on my website!" isIndex>
+    <div className={styles.description}>
+      <h3>
+        <p>
+          As you might have guessed, my name is Gauthier.
+        </p>
+        <p>
+          I'm a young French developer interested in programming various stuff, but mainly things on the web or with JavaScript.
+        </p>
+      </h3>
+    </div>
+    <div className={styles.tilesContainer}>
+      <div className={styles.tiles} onMouseLeave={() => setOveredTile(null)}>
+        <AnimateSharedLayout type="crossfade">
+          {tiles.map((tile, idx) => (
+            <>
+              {tile.blank && <a href={tile.href} target="_blank" onMouseEnter={() => setOveredTile(idx)}>
+                <TileContent {...tile} hoveredTile={hoveredTile === idx} />
+              </a>}
+              {!tile.blank && <Link href={tile.href}>
+                <a onMouseEnter={() => setOveredTile(idx)}>
+                  <TileContent {...tile} hoveredTile={hoveredTile === idx} />
+                </a>
+              </Link>}
+            </>
+          ))}
+        </AnimateSharedLayout>
+      </div>
+    </div>
   </Layout>
-)
+}
 
 export default IndexPage
