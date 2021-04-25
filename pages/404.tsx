f761d5691aff51780a043e4eb556dda406c045db
gauthier-th <mail@gauthier-thomas.dev>
2021-04-25 16:10:19 +0200

    Create 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const AboutPage = () => (
+  <Layout title="Page not found" pageTitle="Page not found">
+    <p>
+      Sorry, but I can't find the page you're looking for.
+      <br />
+      You should probably come back to homepage.
+    </p>
+    <Link href="/">
+      <a className="gradientButton">
+        Home here!!
+      </a>
+    </Link>
+  </Layout>
+)
+
+export default AboutPage
