ee4763a8fc9a6b5e81ad68b3845f038e56e548dc
Gauthier <mail@gauthierth.fr>
2024-05-04 14:52:31 +0200

    Dockerize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import '../styles/globals.css'
 import { useRouter } from 'next/router'
-import type { AppProps } from 'next/app'
 import { AnimateSharedLayout } from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   const { locale, defaultLocale } = useRouter()
   return <AnimateSharedLayout>
     <Component {...pageProps} locale={locale || defaultLocale || 'en'} />
